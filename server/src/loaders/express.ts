2123adab9b880dc069575ba1d2ae34e7367934a6
o-henry <cksgod16@gmail.com>
2020-06-08 12:01:09 +0900

    Add socket server

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -2,3 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import config from "@config/index";
+
+const expressLoader = ({ app }: { app: express.Application }) => {
+  app.get("/", (req, res) => {
+    res.status(200).send("Welcome");
+    res.status(200).end("end");
+  });
+};
+
+export default expressLoader;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,27 @@
-import config from "./config";
-
 import "module-alias/register"; // for absolute paths
+
+import config from "./config";
 
 import express from "express";
 
-const app = express();
+import io from "socket.io";
 
-app.listen(config.port, () => {
-  console.log(`Server listening on port: ${config.port}`);
-});
+async function startServer() {
+  const app = express();
+  const server = require("http").Server(app);
+
+  await require("./loaders").default({ expressApp: app });
+
+  io(server).on("connection", (socket: any) => {
+    console.log("test");
+    socket.on("test", (data: any) => {
+      console.log(data, "print test message");
+    });
+  });
+
+  server.listen(config.port, () => {
+    console.log(`Server listening on port: ${config.port}`);
+  });
+}
+
+startServer();

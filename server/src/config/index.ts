9e0ccf540cd81d6486758e3bfa14a70add4df221
o-henry <cksgod16@gmail.com>
2020-06-22 16:47:00 +0900

    Add heroku to github

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
-const envFound = dotenv.config();
+// const envFound = dotenv.config();
 
-if (envFound.error) {
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+// if (envFound.error) {
+//   throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 
 export default {
   port: process.env.PORT || 8000,

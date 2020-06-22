f6fc70be79d080e70739bb71b476f3f375d3a48d
o-henry <cksgod16@gmail.com>
2020-06-22 16:40:16 +0900

    Server : Fix env

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
 const envFound = dotenv.config();
 
@@ -9,7 +9,7 @@ if (envFound.error) {
 }
 
 export default {
-  port: process.env.PORT,
+  port: process.env.PORT || 8000,
 
   api: {
     prefix: "/api",

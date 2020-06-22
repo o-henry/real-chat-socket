dcebaeb8b29d47403885668b0a1b3797339ceab8
o-henry <cksgod16@gmail.com>
2020-06-22 13:17:02 +0900

    Server: Add Count Connect/Disconnect User

---
+++ server/README.md
@@ -0,0 +1,3 @@
+### Clients Counter List
+
+[Ref](https://stackoverflow.com/questions/10275667/socket-io-connected-user-count)
--- server/src/loaders/socket.ts
+++ server/src/loaders/socket.ts
@@ -8,7 +8,20 @@ const socketServer = ({ server }: { server: e.Application }) => {
     socket.on("chat message", (nickname: string, msg: string) => {
       io.emit("chat message", nickname, msg);
     });
+    socket.on("disconnect", function () {
+      var connectionMessage = "Disconnected 1 User";
+      console.log(connectionMessage);
+    });
+
+    console.log(socket.client.conn.server.clientsCount + " users connected");
   });
+
+  // io.clients((error: any, clients: any) => {
+  //   if (error) throw error;
+  //   console.log("client", clients);
+  // });
+
+  // console.log("counts:", io.sockets.sockets.length);
 };
 
 export default socketServer;
